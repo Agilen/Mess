f479e4aeb882e946ccfb965d90b93a9c382f4a59
Agilen <rusev.denis.v@gmail.com>
2021-12-31 14:55:04 +0200

    Update socket

--- server/socket.go
+++ server/socket.go
@@ -3,27 +3,33 @@ package server
 import (
 	"net"
 	"strconv"
+	"time"
 )
 
 type Socket struct {
-	Port       int
-	LifeTime   int
-	Client     ClientSocketInfo
-	Connection net.Conn
+	LifeTime         int
+	Client           ClientSocketInfo
+	Connection       net.Conn
+	AcceptedAttempts int
+	ErrorChan        chan error
 }
 
 type ClientSocketInfo struct {
+	Port   int
 	Name   string
 	Ipv4   string
 	Number int
 }
 
-func SockCreate(CSI ClientSocketInfo, port int) *Socket {
-	return &Socket{
-		Port:     port,
-		LifeTime: 5000000,
-		Client:   CSI,
+func SockCreate(CSI ClientSocketInfo) *Socket {
+	S := &Socket{
+		LifeTime:         10,
+		Client:           CSI,
+		AcceptedAttempts: 5,
+		ErrorChan:        make(chan error),
 	}
+	S.SockListen()
+	return S
 }
 
 func (S *Socket) SockBind() {
@@ -31,17 +37,21 @@ func (S *Socket) SockBind() {
 }
 
 func (S *Socket) SockListen() error {
-	listener, err := net.Listen("tcp", ":"+strconv.Itoa(S.Port))
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(S.Client.Port))
 	if err != nil {
 		return err
 	}
-	S.SockAccept()
+	S.SockAccept(listener)
 
 	return nil
 }
 
-func (S *Socket) SockAccept() {
-
+func (S *Socket) SockAccept(listener net.Listener) (err error) {
+	S.Connection.SetDeadline(time.Now().Add(time.Second * 2))
+	S.Connection, err = listener.Accept()
+	S.Connection.SetDeadline(time.Now().Add(time.Minute * time.Duration(S.LifeTime)))
+	S.Connection.Write([]byte("connection is successful"))
+	return
 }
 
 func (S *Socket) SockRecv() {
@@ -53,5 +63,6 @@ func (S *Socket) SockSend() {
 }
 
 func (S *Socket) SockClose() {
+	S.Connection.Close()
 
 }
