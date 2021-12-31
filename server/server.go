31877fc15ad290726461e2c87d229fcafdc10887
Agilen <rusev.denis.v@gmail.com>
2021-12-31 14:54:30 +0200

    Change socket storage and add error handeler

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net"
 	"time"
@@ -10,31 +11,28 @@ import (
 var ()
 
 type (
-	serverContext struct {
+	ServerContext struct {
 		ServerName string
 		ServerIpv4 string
 		Ports      map[int]bool
-		Sockets    []*Socket
-	}
-
-	ConnWithUser struct {
-		Port int
-		Conn net.Conn
+		Sockets    map[string]*Socket
+		ErrorChan  chan error
 	}
 )
 
-func ListenAndServe() error {
+func ListenAndServe() {
 	SC := NewServerContext()
 	go SC.ListenClientWish()
-
-	return nil
+	HadnleError(<-SC.ErrorChan)
 }
 
-func NewServerContext() *serverContext {
-	return &serverContext{
+func NewServerContext() *ServerContext {
+	return &ServerContext{
 		ServerName: "SuperPuperServer",
 		ServerIpv4: "172.16.9.120",
 		Ports:      PrepPortsMap(),
+		Sockets:    PrepSocketMap(),
+		ErrorChan:  make(chan error),
 	}
 }
 
@@ -46,7 +44,11 @@ func PrepPortsMap() map[int]bool {
 	return m
 }
 
-func TakeFreePort(server *serverContext) int {
+func PrepSocketMap() map[string]*Socket {
+	return make(map[string]*Socket)
+}
+
+func TakeFreePort(server *ServerContext) int {
 	for i := 3000; i < 65536; i++ {
 		if !server.Ports[i] {
 			server.Ports[i] = true
@@ -56,47 +58,50 @@ func TakeFreePort(server *serverContext) int {
 	return 0
 }
 
-func (SC *serverContext) ListenClientWish() {
+func (SC *ServerContext) ListenClientWish() {
 
 	listener, err := net.Listen("tcp", ":3333")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	for {
+		//ждём подключения
 		connection, err := listener.Accept()
 		if err != nil {
-			break
+			SC.ErrorChan <- err
+			continue
 		}
 
-		BaseInfo, err := json.Marshal(SC)
+		BaseInfo, err := json.Marshal(SC.Ports) //маршалим карту с портами
 		if err != nil {
-			log.Println(err.Error())
-			break
+			SC.ErrorChan <- err
+			continue
 		}
 
-		_, err = connection.Write(BaseInfo)
+		_, err = connection.Write(BaseInfo) //отправлем карту
 		if err != nil {
-			connection.Close()
-			log.Println(err.Error())
-			break
+			SC.ErrorChan <- err
+			continue
 		}
-		//////////////////////////////////////////////////////
+
 		buffer := make([]byte, 2048)
-
-		connection.SetReadDeadline(time.Now().Add(time.Second * 1))
+		////////////////////////////////////////////////////////
+		connection.SetReadDeadline(time.Now().Add(time.Millisecond * 500)) //ждем ответа от клиента, если ответа нет, то рвем подключение и ждем новое
 
 		_, err = connection.Read(buffer)
 		if err != nil {
-			connection.Close()
-			log.Println(err.Error())
-			break
+			if err.Error() == "EOF" {
+				SC.ErrorChan <- err
+				continue
+			}
+			SC.ErrorChan <- err
+			continue
 		}
 		CSI := new(ClientSocketInfo)
 		err = json.Unmarshal(buffer, &CSI)
 		if err != nil {
-			connection.Close()
-			log.Println(err.Error())
-			break
+			SC.ErrorChan <- err
+			continue
 		}
 		////////////////////////////////////////////////////////
 		CSI.Number = len(SC.Sockets)
@@ -104,6 +109,15 @@ func (SC *serverContext) ListenClientWish() {
 		connection.Close()
 
 		SC.Ports[int(buffer[0])] = true
-		SC.Sockets = append(SC.Sockets, SockCreate(*CSI, int(buffer[0])))
+		SC.Sockets[CSI.Name] = SockCreate(*CSI)
+	}
+}
+
+func HadnleError(ch error) {
+	for {
+		if ch != nil {
+			fmt.Println(ch)
+			ch = nil
+		}
 	}
 }
